076ab047fad06a18fac12163663607ee44e4293c
kingle1024 <teran1024@naver.com>
2023-02-06 22:36:44 +0900

    [docs] slf4j 추가

--- src/main/java/com/dz/security/PrincipalDetails.java
+++ src/main/java/com/dz/security/PrincipalDetails.java
@@ -4,10 +4,12 @@ import java.util.Collection;
 
 import com.dz.member.vo.MemberVO;
 import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 @Data
+@Slf4j
 public class PrincipalDetails implements UserDetails{
 
     private MemberVO member;
@@ -52,6 +54,7 @@ public class PrincipalDetails implements UserDetails{
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
+        log.info("");
         Collection<GrantedAuthority> authList = new ArrayList<>();
 
         member.getRoles().forEach(r -> authList.add(() -> r));
