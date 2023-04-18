2779dc64786bf5b018fb5134411867f1be86ac2d
kingle1024 <teran1024@naver.com>
2023-04-18 22:56:03 +0900

    feat: test 이름 변경

--- src/test/java/com/dz/member/service/ProMemberServiceImplTest.java
+++ src/test/java/com/dz/member/service/ProMemberServiceImplTest.java
@@ -19,7 +19,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @SpringBootTest
 @AutoConfigureMockMvc
 @Transactional
-class MemberServiceImplTest {
+class ProMemberServiceImplTest {
     @Autowired
     MemberService memberService;
     @Autowired
